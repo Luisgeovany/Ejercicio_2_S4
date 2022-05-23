123bd475df8194e2269300f98ef4c82da97cf613
PERSONAL <glopezvan@gmail.com>
2022-05-23 17:04:06 -0500

    mejora al ejercicio 2

--- Ejercicio_2_S4.py
+++ Ejercicio_2_S4.py
@@ -1,5 +1,15 @@
 #mostrar todos los números impares desde un número de inicio y otro final.
 
-for n in range(1,10):
+numInicial = int(input('Ingrese el numero inicial: '))
+numFinal = int(input('Ingrese el numero Final: '))
+
+numerosImpar = []
+
+while numFinal <= numInicial:
+    numFinal = int(input('El segundo número debe ser mayor que el primero. Inroduce otro número: '))
+for n in range(numInicial, numFinal + 1):
     if n % 2 != 0:
-        print(f'{n} es impar')
+        numerosImpar.append(n)
+
+print(f"Lista de Números impares entre {numInicial} y {numFinal}:")
+print(numerosImpar)
